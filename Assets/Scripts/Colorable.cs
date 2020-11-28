aaf0d8f45d6b4c480f8a150ea408096171cea6af
Andrew Lott <AndrewLott@gmail.com>
2020-11-28 02:09:40 -0800

    Add colorable component to player and planet prefabs

---
+++ Assets/Scripts/Colorable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colorable : MonoBehaviour {
+    [SerializeField] public Color color = Color.white;
+    private Color prevColor = Color.white;
+    [SerializeField] private List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
+
+    private void Start() {
+        SetSpriteColor();
+    }
+
+    private void Update() {
+        if (prevColor != color) {
+            SetSpriteColor();
+        }
+    }
+
+    private void SetSpriteColor() {
+        foreach(SpriteRenderer spriteRenderer in spriteRenderers) {
+            spriteRenderer.color = color;
+        }
+        prevColor = color;
+    }
+}
